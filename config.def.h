5e4db4cf1b80aa437a70c594c13647a4d4adfac4
Hendry Khoza <h3ndry@users.noreply.github.com>
2023-02-23 21:32:22 +0200

    correct colores

--- config.def.h
+++ config.def.h
@@ -5,16 +5,16 @@ static int topbar = 1;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"JetBrainsMono-Regular:size=11"
+	"JetBrainsMono-Regular:size=12"
 };
 static const unsigned int alpha = 0xf0;
 static const char *prompt      = "";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#a9b1d6", "#121212" },
+	[SchemeNorm] = { "#a9b1d6", "#0d1117" },
 	[SchemeSel] = { "#c0caf5", "#005577" },
 	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
+	[SchemeNormHighlight] = { "#ffc978", "#0d1117" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 
